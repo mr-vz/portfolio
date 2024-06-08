1319770c204523835c5eb1888220bae2cbb5497c
Vladimir Zaharov <vladimirzaharov94@gmail.com>
2024-06-08 23:54:43 +0700

    Styled Footer

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from "styled-components";
 import {Icon} from "../../components/icon/Icon";
 import {FlexWrapper} from "../../components/FlexWrapper";
+import {theme} from "../../styles/Theme";
 
 export const Footer = () => {
     return (
@@ -37,22 +38,43 @@ export const Footer = () => {
 };
 
 const StyledFooter = styled.footer`
-    background-color: darkkhaki;
-    min-height: 20vh;
+    background-color: ${theme.colors.primaryBg};
+    padding: 40px 0;
 `
 const Name = styled.span`
-    
+    font-family: 'Josefin Sans', sans-serif;
+    font-weight: 700;
+    font-size: 22px;
+    letter-spacing: 3px;
 `
 const SocialList = styled.ul`
     display: flex;
-    gap: 30px;
+    gap: 20px;
+    margin: 30px 0;
 `
 const SocialItem = styled.li`
     
 `
 const SocialLink = styled.a`
+    border-radius: 50%;
+    width: 35px;
+    height: 35px;
+    background-color: rgba(255, 255, 255, 0.1);
+    display: flex;
+    justify-content: center;
+    align-items: center;
     
+    color: ${theme.colors.accent};
+    
+    &:hover {
+        color: ${theme.colors.primaryBg};
+        transform: translateY(-4px);
+        background-color: ${theme.colors.accent};
+    }
 `
 const Copyright = styled.small`
-    
+    font-weight: 400;
+    font-size: 12px;
+    text-align: center;
+    opacity: 0.5;
 `

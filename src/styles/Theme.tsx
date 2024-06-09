bf037fd60bc53696d27dc658532b0a41ab6da1cf
Vladimir Zaharov <vladimirzaharov94@gmail.com>
2024-06-09 15:28:39 +0700

    Header Adaptive

--- src/styles/Theme.tsx
+++ src/styles/Theme.tsx
@@ -9,5 +9,10 @@ export const theme = {
         borderColor: '#4a4a4a',
 
         placeholderColor: '#495057'
+    },
+
+    media: {
+        tablet: 'screen and (max-width: 768px)',
+        mobile: 'screen and (max-width: 576px)'
     }
 }

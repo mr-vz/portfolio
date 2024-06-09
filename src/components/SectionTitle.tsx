91f83dead12ce93afbb9bcec5a40a53991b9b852
Vladimir Zaharov <vladimirzaharov94@gmail.com>
2024-06-09 18:25:13 +0700

    Skills Adaptive

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,12 +1,12 @@
 import styled from "styled-components";
 import {theme} from "../styles/Theme";
+import {font} from "../styles/Common";
 
 export const SectionTitle = styled.h2`
+    ${font({family: "'Josefin Sans', sans-serif", weight: 600, Fmax: 36, Fmin: 30})}
+    
     text-align: center;
-    font-family: 'Josefin Sans', sans-serif;
-    font-weight: 600;
-    font-size: 36px;
-    letter-spacing: 0.14em;
+    letter-spacing: 5px;
     margin-bottom: 90px;
     
     position: relative;
@@ -21,5 +21,9 @@ export const SectionTitle = styled.h2`
         left: 50%;
         transform: translateX(-50%);
         bottom: -30px;
+        
+        @media ${theme.media.mobile} {
+            bottom: -24px;
+        }
     }
 `

2625c1be64923a32e4c6daafbd03fee8121340b3
Vladimir Zaharov <vladimirzaharov94@gmail.com>
2024-06-10 16:06:06 +0700

    Skills Refactoring

--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -1,40 +1,56 @@
 import React from 'react';
-import styled from "styled-components";
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import { SectionTitle } from '../../../components/SectionTitle';
 import {Skill} from "./skill/Skill";
 import {Container} from "../../../components/Container";
+import { S } from './Skills_Styles';
+
+const skillData = [
+    {
+        iconId: 'code',
+        title: 'Html5',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'
+    },
+    {
+        iconId: 'css',
+        title: 'Css3',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'
+    },
+    {
+        iconId: 'react',
+        title: 'React',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'
+    },
+    {
+        iconId: 'typescript',
+        title: 'TypeScript',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'
+    },
+    {
+        iconId: 'styledComponents',
+        title: 'Styled components',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'
+    },
+    {
+        iconId: 'figma',
+        title: 'Web design',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'
+    }
+]
 
 export const Skills = () => {
     return (
-        <StyledSkills>
+        <S.Skills>
             <Container>
                 <SectionTitle>My Skills</SectionTitle>
                 <FlexWrapper wrap={'wrap'} justify={'space-between'}>
-                    <Skill iconId={'code'}
-                           title={'Html 5'}
-                           description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'}/>
-                    <Skill iconId={'css'}
-                           title={'Css 3'}
-                           description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'}/>
-                    <Skill iconId={'react'}
-                           title={'React'}
-                           description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'}/>
-                    <Skill iconId={'typescript'}
-                           title={'Typescript'}
-                           description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'}/>
-                    <Skill iconId={'styledComponents'}
-                           title={'Styled components'}
-                           description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'}/>
-                    <Skill iconId={'figma'}
-                           title={'Web design'}
-                           description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'}/>
+                    {skillData.map((s, index) => {
+                        return  <Skill iconId={s.iconId} key={index}
+                                       title={s.title}
+                                       description={s.description}/>
+                    })}
                 </FlexWrapper>
             </Container>
-        </StyledSkills>
+        </S.Skills>
     );
 };
-
-const StyledSkills = styled.section`
-
-`
--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -4,8 +4,9 @@ import {SectionTitle} from "../../../components/SectionTitle";
 import {Icon} from "../../../components/icon/Icon";
 import {Slider} from "../../../components/slider/Slider";
 import { FlexWrapper } from '../../../components/FlexWrapper';
-import {IconWrapper} from "../skills/skill/Skill";
+
 import {Container} from "../../../components/Container";
+import { S } from '../skills/Skills_Styles';
 
 export const Testimony = () => {
     return (
@@ -13,9 +14,9 @@ export const Testimony = () => {
             <Container>
                 <SectionTitle>Testimony</SectionTitle>
                 <FlexWrapper direction={'column'} align={'center'}>
-                    <IconWrapper>
+                    <S.IconWrapper>
                         <Icon iconId={'quote'}/>
-                    </IconWrapper>
+                    </S.IconWrapper>
                     <Slider/>
                 </FlexWrapper>
             </Container>
@@ -27,7 +28,7 @@ const StyledTestimony = styled.section`
     min-height: 50vh;
     background-color: #c198e8;
     
-    ${IconWrapper} {
+    ${S.IconWrapper} {
         margin: 28px 0 72px;
     }
 `

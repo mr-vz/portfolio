f17b1e999028bfce9ce226a641f7d23152aff251
Vladimir Zaharov <vladimirzaharov94@gmail.com>
2024-06-10 15:24:50 +0700

    Header Refactoring

---
+++ src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { S } from '../HeaderMenu_Styles';
+import {Menu} from "../menu/Menu";
+
+export const MobileMenu: React.FC<{menuItems: Array<string>}> = (props: {menuItems: Array<string>}) => {
+    return (
+        <S.MobileMenu>
+            <S.BurgerButton isOpen={false}>
+                <span></span>
+            </S.BurgerButton>
+            <S.MobileMenuPopup isOpen={false}>
+                <Menu menuItems={props.menuItems}/>
+            </S.MobileMenuPopup>
+        </S.MobileMenu>
+    );
+};

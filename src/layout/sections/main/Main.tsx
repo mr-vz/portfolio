e2e95b31234489c40ffef83f4fcbe57f975d494b
Vladimir Zaharov <vladimirzaharov94@gmail.com>
2024-06-10 14:24:09 +0700

    Pixel perfect

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -35,7 +35,6 @@ const StyledMain = styled.section`
 const PhotoWrapper = styled.div`
     position: relative;
     z-index: 0;
-    margin-top: 65px;
     
     &::before {
         content: '';
@@ -53,6 +52,10 @@ const PhotoWrapper = styled.div`
             top: -17px;
             left: 20px;
         }
+    }
+
+    @media ${theme.media.mobile} {
+        margin-top: 65px;
     }
 `
 

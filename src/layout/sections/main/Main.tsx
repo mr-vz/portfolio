467d43815ee864fdd9375cbdd11b76cd8a2a7faf
Vladimir Zaharov <vladimirzaharov94@gmail.com>
2024-06-14 22:37:28 +0700

    Add Parallax effect to photo in Main

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -4,6 +4,7 @@ import {FlexWrapper} from "../../../components/FlexWrapper";
 import {Container} from "../../../components/Container";
 import { S } from './Main_Styles';
 import Typewriter from 'typewriter-effect';
+import Tilt from 'react-parallax-tilt';
 
 export const Main: React.FC = () => {
     return (
@@ -13,7 +14,6 @@ export const Main: React.FC = () => {
                     <div>
                         <S.SmallText>Hi There</S.SmallText>
                         <S.Name>I am <span>Vladimir Zaharov</span></S.Name>
-                        {/*<S.MainTitle>A Frontend Developer. </S.MainTitle>*/}
                         <S.MainTitle>
                             <p>A Frontend Developer.</p>
                             <Typewriter
@@ -28,9 +28,19 @@ export const Main: React.FC = () => {
 
                     </div>
 
-                    <S.PhotoWrapper>
-                        <S.Photo src={photo} alt=""/>
-                    </S.PhotoWrapper>
+                    <Tilt
+                        className="parallax-effect-img"
+                        tiltMaxAngleX={40}
+                        tiltMaxAngleY={40}
+                        perspective={800}
+                        transitionSpeed={1500}
+                        scale={1.1}
+                        gyroscope={true}
+                    >
+                        <S.PhotoWrapper>
+                            <S.Photo src={photo} alt=""/>
+                        </S.PhotoWrapper>
+                    </Tilt>
                 </FlexWrapper>
             </Container>
         </S.Main>

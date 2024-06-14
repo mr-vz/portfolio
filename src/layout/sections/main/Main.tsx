54d00afb0c9ac808683cbc1e4078c30b1b58686a
Vladimir Zaharov <vladimirzaharov94@gmail.com>
2024-06-14 22:26:48 +0700

    Add typewritter effect in Main

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -3,6 +3,7 @@ import photo from '../../../assets/images/photo.jpg'
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import {Container} from "../../../components/Container";
 import { S } from './Main_Styles';
+import Typewriter from 'typewriter-effect';
 
 export const Main: React.FC = () => {
     return (
@@ -12,7 +13,19 @@ export const Main: React.FC = () => {
                     <div>
                         <S.SmallText>Hi There</S.SmallText>
                         <S.Name>I am <span>Vladimir Zaharov</span></S.Name>
-                        <S.MainTitle>A Frontend Developer. </S.MainTitle>
+                        {/*<S.MainTitle>A Frontend Developer. </S.MainTitle>*/}
+                        <S.MainTitle>
+                            <p>A Frontend Developer.</p>
+                            <Typewriter
+                                options={{
+                                    strings: ['A Frontend Developer.'],
+                                    autoStart: true,
+                                    loop: true,
+                                    delay: 100
+                                }}
+                            />
+                        </S.MainTitle>
+
                     </div>
 
                     <S.PhotoWrapper>
--- src/layout/sections/main/Main_Styles.ts
+++ src/layout/sections/main/Main_Styles.ts
@@ -49,6 +49,10 @@ const Photo = styled.img`
 
 const MainTitle = styled.h1`
     ${font({weight: 400, Fmax: 27, Fmin: 20})};
+    
+    p {
+        display: none;
+    }
 `
 
 const Name = styled.h2`
